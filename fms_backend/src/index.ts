420b6a50bf325c47f0a521e05aa470aea19aaa4c
saatvik-10 <saatvikmadan529@gmail.com>
2026-04-15 11:08:22 +0530

    added route

--- fms_backend/src/index.ts
+++ fms_backend/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { cors } from 'hono/cors';
 import { serve } from '@hono/node-server';
+import router from './routes/index.route';
 
 const port = process.env.PORT;
 
@@ -13,6 +14,8 @@ app.use(cors());
 app.get('/health', (c) => {
   return c.json({ status: 'ok' });
 });
+
+app.route('/', router);
 
 app.notFound((c) => {
   return c.json({ err: 'Page not found' }, 404);
