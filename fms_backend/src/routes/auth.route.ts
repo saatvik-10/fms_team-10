ee26ab7070d4cdb46113de590e7ad296ee9897ec
saatvik-10 <saatvikmadan529@gmail.com>
2026-04-15 11:08:58 +0530

    added auth routes

---
+++ fms_backend/src/routes/auth.route.ts
@@ -0,0 +1,12 @@
+import { Hono } from 'hono';
+import { proxyAuth, requireRole } from '../proxy';
+import { Auth } from '../controllers/auth.controller.ts';
+
+const authRoute = new Hono();
+const controller = new Auth();
+
+authRoute.post('/super-admin/login', controller.login);
+authRoute.post('/managers', proxyAuth, requireRole('SUPER_ADMIN'), controller.createManager);
+authRoute.get('/me', proxyAuth, controller.getMe);
+
+export default authRoute;
