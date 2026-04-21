6dee46818f9583cfbc10b99336fbe82e232168d4
saatvik-10 <saatvikmadan529@gmail.com>
2026-04-22 00:05:47 +0530

    otp send and verification routes

--- fms_backend/src/routes/auth.route.ts
+++ fms_backend/src/routes/auth.route.ts
@@ -1,13 +1,14 @@
 import { Hono } from 'hono';
 import { proxyAuth } from '../proxy';
 import { Auth } from '../controllers/auth.controller.ts';
-import { sendCredentialsMail } from '../services/resend';
 
 const authRoute = new Hono();
 const controller = new Auth();
 
 authRoute.post('/super-admin/signin', controller.signin);
 authRoute.post('/signin', controller.userSignin);
+authRoute.post('/otp/send', controller.sendOtpMail);
+authRoute.post('/verify-otp', controller.verifyOtp);
 authRoute.get('/profile', proxyAuth, controller.getProfile);
 
 export default authRoute;
