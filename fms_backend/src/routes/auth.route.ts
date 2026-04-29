4622d3a18af57802997f48445bd17b52ef24dc57
saatvik-10 <saatvikmadan529@gmail.com>
2026-04-29 09:16:29 +0530

    reverted to otp

--- fms_backend/src/routes/auth.route.ts
+++ fms_backend/src/routes/auth.route.ts
@@ -7,8 +7,8 @@ const controller = new Auth();
 
 authRoute.post('/super-admin/signin', controller.signin);
 authRoute.post('/signin', controller.userSignin);
-// authRoute.post('/otp/send', controller.sendOtpMail);
-// authRoute.post('/verify-otp', controller.verifyOtp);
+authRoute.post('/otp/send', controller.sendOtpMail);
+authRoute.post('/verify-otp', controller.verifyOtp);
 authRoute.get('/profile', proxyAuth, controller.getProfile);
 
 export default authRoute;
